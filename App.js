eb1a424d5a7bbda4251a0488d80bf130121faee3
Daniel <drsoper@gmail.com>
2018-03-04 05:42:00 -0800

    fetch emails from server and display

--- App.js
+++ App.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Button, Text, View, FlatList, ScrollView } from 'react-native';
+import { Card } from 'native-base';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emails: [],
+      openEmail: {}
+    };
+    //this.fetchEmails = this.fetchEmails.bind(this);
+  }
+
+  componentWillMount(){
+    fetch('http://d3.llip.life/emails?authUser=drsoper&emailId=drsoper@garagescript.org&offset=0&count=10')
+      .then((response)=> {
+        return response.json();
+      })
+      .then((emailResponse)=> {
+        this.setState({emails: emailResponse});
+      }).catch(error=> console.log('error', error));
+  }
+  openEmail(openEmailData) {
+    this.setState({
+      openEmail: openEmailData 
+    }); 
+  }
+
   render() {
+    const emailRecipients = this.state.emails.map((email, i)=> {
+      return(
+        <Card style={styles.emailContainer} onPress={()=> this.openEmail(email)} key={i}>
+          <Text>
+            {email.recipient}
+          </Text>
+        </Card>
+      ); 
+    });
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <ScrollView>
+          {emailRecipients}
+        </ScrollView>
       </View>
     );
   }
@@ -16,8 +50,13 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: 'blue',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
   },
+  emailContainer: {
+    flex: 1,
+  }
+
 });
